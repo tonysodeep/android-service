ba5e13e0018e66a96649f6bc2486c1e169050859
tonysodeep <hynguyen2727@gmail.com>
2021-12-30 10:13:47 +0700

    first commit

---
+++ app/src/main/java/com/example/appservices/MainActivity2.java
@@ -0,0 +1,34 @@
+package com.example.appservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button = findViewById(R.id.buttonStop);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        startService(new Intent(this,MyService.class));
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(new Intent(this,MyService.class));
+    }
+}
